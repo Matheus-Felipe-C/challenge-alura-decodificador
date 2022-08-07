1353fb0b2a712347e08d2243e07e540c51447faa
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-08-07 15:31:09 +0000

    Added static Result area

--- decodificador/src/App.jsx
+++ decodificador/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import TextInput from './assets/components/Textinput/TextInput'
 import Button from './assets/components/Button/Button'
+import Result from './assets/components/Result/Result'
 
 function App() {
 
   return (
     <div className='container'>
       <TextInput />
+      <Result />
       <div className="btn-container">
         <Button title='Criptografar' color='blue' />
         <Button title='Descriptografar' color='grey' />
---
+++ decodificador/src/assets/components/Result/Result.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+import notFound from '../../img/not-found.svg'
+
+export default function Result() {
+    return (
+        <div className='result'>
+            <img src={notFound} alt="" className="notFound" />
+            <p><strong>Nenhuma mensagem encontrada</strong></p>
+            <p>Digite um texto que você deseja criptografar ou descriptografar.</p>
+        </div>
+    )
+}
