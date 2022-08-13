523fe730d383c9eebdb13a49216b14d4956a0bbf
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-08-13 12:10:49 +0000

    Adicionado verificação de input vazio

--- versao-ts/src/encriptador.ts
+++ versao-ts/src/encriptador.ts
@@ -1,7 +1,12 @@
 let result = document.getElementById('result')
+let input = document.getElementById('textInput') as HTMLInputElement
 
-function oi() {
-    if (result) {
-        result.innerHTML = 'oi'
+function encriptador() {
+    if (result && input) {
+        if (input.value.length == 0) {
+            result.innerHTML = '<img src="./src/img/not-found.svg" alt="Texto não encontrado"><p><strong>Nenhuma mensagem encontrada</strong></p><p>Digite um texto que você deseja criptografar ou descriptografar.</p>'
+        } else {
+            result.innerHTML = input.value
+        }
     }
 }
