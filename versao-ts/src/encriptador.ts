5cf38dafe885535505b6d3eed00cfb3681b63db0
Matheus Felipe de Oliveira Ferreira <theusf40@hotmail.com>
2022-08-15 00:57:50 +0000

    Refatorado a verificação de texto vazio para ficar mais legível

--- versao-ts/src/encriptador.ts
+++ versao-ts/src/encriptador.ts
@@ -4,7 +4,10 @@ let input = document.getElementById('textInput') as HTMLInputElement
 function encriptar() {
     if (result && input) {
         if (input.value.length == 0) {
-            result.innerHTML = '<img src="./src/img/not-found.svg" alt="Texto não encontrado"><p><strong>Nenhuma mensagem encontrada</strong></p><p>Digite um texto que você deseja criptografar ou descriptografar.</p>'
+            result.innerHTML = `
+                <img src="./src/img/not-found.svg" alt="Texto não encontrado">
+                <p><strong>Nenhuma mensagem encontrada</strong></p>
+                <p>Digite um texto que você deseja criptografar ou descriptografar.</p>`
         } else {
             let text = input.value
 
